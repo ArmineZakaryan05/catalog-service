caabfe0be73b6dc5fd5ddea0c2da63bbef83b16a
User <zakaryanarmine991@gmail.com>
2025-04-16 20:01:41 +0400

    Change greeting

--- src/main/resources/templates/db.migration/V1__Initial_schema.sql
+++ src/main/resources/templates/db.migration/V1__Initial_schema.sql
@@ -7,6 +7,4 @@ CREATE TABLE book
     title              varchar(255)          NOT NULL,
     created_date       timestamp NOT NULL,
     last_modified_date timestamp NOT NULL,
-    version            integer NOT NULL ,
-     publisher VARCHAR(255)
-);
+    version            integer NOT NULL );
