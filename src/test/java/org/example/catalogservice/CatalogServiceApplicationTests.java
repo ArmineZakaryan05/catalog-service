63a9023be247f05c342e5d951d101ba5b54a5eb1
User <zakaryanarmine991@gmail.com>
2025-04-16 19:23:14 +0400

    Change greeting

--- src/test/java/org/example/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/org/example/catalogservice/CatalogServiceApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test")
+@ActiveProfiles("integration")
 class CatalogServiceApplicationTests {
 
     @Autowired
